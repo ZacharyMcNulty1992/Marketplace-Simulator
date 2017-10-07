79a0fa04d6787c31b4e1ac98429db9ae0b2c98de
Zachary McNulty <zach.mcnulty1992@gmail.com>
2017-10-07 19:37:45 -0400

    reformat the project to make things a bit easier

---
+++ src/app/app.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"actor"
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Initializing World\n")
+	//TODO: create a world map
+
+	//TODO: change this so that it is creating a lot of new people.
+	person := actor.NewPerson("merchant")
+	//TODO: have the main thread be the thread that controls world events.
+
+}
